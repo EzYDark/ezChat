87e5320ec7264b5fda545d914e66a90ea985905a
EzYDark <ezydark@protonmail.com>
2025-10-28 17:16:15 +0100

    init

---
+++ shell.nix
@@ -0,0 +1,38 @@
+# Run with `nix-shell shell.nix`
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    gobject-introspection
+    cargo 
+    cargo-tauri # Optional, Only needed if Tauri doesn't work through the traditional way.
+    nodejs # Optional, this is for if you have a js frontend
+    gcc
+    llvmPackages.lld
+    fish
+  ];
+
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
+  ];
+  shellHook = ''
+    export NIX_ENFORCE_PURITY=0
+    export SHELL=${pkgs.fish}/bin/fish
+    if [ -z "$FISH_VERSION" ] && [ -n "$PS1" ]; then
+      exec ${pkgs.fish}/bin/fish
+    fi
+  '';
+}
