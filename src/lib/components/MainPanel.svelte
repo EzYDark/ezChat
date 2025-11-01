a574e8c00fd7f7fd7138d7e3ff1d4fb6c93b34d2
EzYDark <ezydark@protonmail.com>
2025-11-01 12:58:50 +0100

    Right panel closed button

--- src/lib/components/MainPanel.svelte
+++ src/lib/components/MainPanel.svelte
@@ -2,12 +2,18 @@
 	import MessageBubble from "$lib/components/MessageBubble.svelte";
 	import Seperator from "$lib/components/Seperator.svelte";
 	import InputPanel from "$lib/components/InputPanel.svelte";
+    import { ArrowLeftFromLine } from "@lucide/svelte";
 
 	export let onSend: () => void = () => {};
 </script>
 
 <div class="flex flex-col flex-1">
-	<div class="h-full flex flex-col flex-1 gap-2 overflow-y-auto custom-scrollbar">
+	<div class="h-full flex flex-col flex-1 gap-2 overflow-y-auto custom-scrollbar relative">
+		<div class="absolute right-2 top-1 flex">
+			<div class="w-10 h-10 flex justify-center items-center rounded-md hover:bg-neutral-700 active:bg-neutral-600 cursor-pointer">
+				<ArrowLeftFromLine size="20" strokeWidth="1"/>
+			</div>
+		</div>
 		<MessageBubble
 			sender="Kheper"
 			text="Hello. I'm Kheper... How can I help you today? Is there anything specific you'd like to discuss? I'm here to assist you with a wide range of topics and questions. Feel free to ask me anything!"
