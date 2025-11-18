44480a1434e1da639aec87edc7dfb53114a7f7d2
EzYDark <ezydark@protonmail.com>
2025-11-18 02:16:11 +0100

    Added Android support and assets

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#[cfg(target_os = "android")]
+use std::error::Error;
+
+#[cfg(target_os = "android")]
+slint::include_modules!();
+
+#[cfg(target_os = "android")]
+#[unsafe(no_mangle)]
+fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn Error>> {
+    slint::android::init(app).unwrap();
+
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()?;
+
+    Ok(())
+}
