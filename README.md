41cebb0eeea865c48061d3cdcfe2dc5f1ddb112a
EzYDark <ezydark@protonmail.com>
2025-11-19 23:57:30 +0100

    Base + simple dropdown menu

---
+++ README.md
@@ -0,0 +1,9 @@
+# Desktop
+```ps1
+$env:SLINT_LIVE_PREVIEW = "1"; cargo run
+```
+
+# Android
+```sh
+cargo apk run --target aarch64-linux-android --lib
+```
--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,13 @@ fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn Error>> {
 
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
+    // ui.on_request_increase_value({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         ui.set_counter(ui.get_counter() + 1);
+    //     }
+    // });
 
     ui.run()?;
 
